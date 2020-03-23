09b331d5e15a1266fc3e863fae099da880126dc2
JanCodingg <janhubmann257@gmail.com>
2020-03-23 17:33:21 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Tic_tac_to
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        public void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.A0 = new System.Windows.Forms.Button();
+            this.B0 = new System.Windows.Forms.Button();
+            this.C0 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.lbl_spieler2 = new System.Windows.Forms.TextBox();
+            this.lbl_Spieler1 = new System.Windows.Forms.TextBox();
+            this.lbl_Gleichstand = new System.Windows.Forms.TextBox();
+            this.neuesSpiel = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Beenden = new System.Windows.Forms.Button();
+            this.A111 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // A0
+            // 
+            this.A0.BackColor = System.Drawing.Color.White;
+            this.A0.Location = new System.Drawing.Point(26, 22);
+            this.A0.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.A0.Name = "A0";
+            this.A0.Size = new System.Drawing.Size(217, 183);
+            this.A0.TabIndex = 8;
+            this.A0.UseVisualStyleBackColor = false;
+            this.A0.Click += new System.EventHandler(this.buttonClickt);
+            // 
+            // B0
+            // 
+            this.B0.BackColor = System.Drawing.Color.White;
+            this.B0.Location = new System.Drawing.Point(26, 218);
+            this.B0.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.B0.Name = "B0";
+            this.B0.Size = new System.Drawing.Size(217, 183);
+            this.B0.TabIndex = 9;
+            this.B0.UseVisualStyleBackColor = false;
+            this.B0.Click += new System.EventHandler(this.buttonClickt);
+            // 
+            // C0
+            // 
+            this.C0.BackColor = System.Drawing.Color.White;
+            this.C0.Location = new System.Drawing.Point(26, 413);
+            this.C0.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.C0.Name = "C0";
+            this.C0.Size = new System.Drawing.Size(217, 183);
+            this.C0.TabIndex = 10;
+            this.C0.UseVisualStyleBackColor = false;
+            this.C0.Click += new System.EventHandler(this.buttonClickt);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.White;
+            this.C1.Location = new System.Drawing.Point(256, 413);
+            this.C1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(217, 183);
+            this.C1.TabIndex = 13;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.buttonClickt);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.White;
+            this.B1.Location = new System.Drawing.Point(256, 218);
+            this.B1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(217, 183);
+            this.B1.TabIndex = 12;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.buttonClickt);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.SystemColors.Window;
+            this.C2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.C2.Location = new System.Drawing.Point(485, 413);
+            this.C2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(217, 183);
+            this.C2.TabIndex = 16;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.UseWaitCursor = true;
+            this.C2.Click += new System.EventHandler(this.buttonClickt);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.White;
+            this.B2.Location = new System.Drawing.Point(485, 218);
+            this.B2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(217, 183);
+            this.B2.TabIndex = 15;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.buttonClickt);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.White;
+            this.A2.Location = new System.Drawing.Point(485, 22);
+            this.A2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(217, 183);
+            this.A2.TabIndex = 14;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.buttonClickt);
+            // 
+            // lbl_spieler2
+            // 
+            this.lbl_spieler2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lbl_spieler2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbl_spieler2.Enabled = false;
+            this.lbl_spieler2.ForeColor = System.Drawing.Color.Black;
+            this.lbl_spieler2.Location = new System.Drawing.Point(715, 98);
+            this.lbl_spieler2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.lbl_spieler2.Name = "lbl_spieler2";
+            this.lbl_spieler2.Size = new System.Drawing.Size(288, 25);
+            this.lbl_spieler2.TabIndex = 17;
+            // 
+            // lbl_Spieler1
+            // 
+            this.lbl_Spieler1.BackColor = System.Drawing.Color.DarkGray;
+            this.lbl_Spieler1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbl_Spieler1.Enabled = false;
+            this.lbl_Spieler1.ForeColor = System.Drawing.Color.Black;
+            this.lbl_Spieler1.Location = new System.Drawing.Point(715, 22);
+            this.lbl_Spieler1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.lbl_Spieler1.Name = "lbl_Spieler1";
+            this.lbl_Spieler1.Size = new System.Drawing.Size(288, 25);
+            this.lbl_Spieler1.TabIndex = 18;
+            // 
+            // lbl_Gleichstand
+            // 
+            this.lbl_Gleichstand.BackColor = System.Drawing.Color.Silver;
+            this.lbl_Gleichstand.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbl_Gleichstand.Enabled = false;
+            this.lbl_Gleichstand.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.lbl_Gleichstand.Location = new System.Drawing.Point(715, 218);
+            this.lbl_Gleichstand.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.lbl_Gleichstand.Name = "lbl_Gleichstand";
+            this.lbl_Gleichstand.Size = new System.Drawing.Size(364, 25);
+            this.lbl_Gleichstand.TabIndex = 19;
+            this.lbl_Gleichstand.Text = "Gleichstand";
+            // 
+            // neuesSpiel
+            // 
+            this.neuesSpiel.Location = new System.Drawing.Point(715, 310);
+            this.neuesSpiel.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.neuesSpiel.Name = "neuesSpiel";
+            this.neuesSpiel.Size = new System.Drawing.Size(256, 87);
+            this.neuesSpiel.TabIndex = 20;
+            this.neuesSpiel.Text = "Neues Spiel";
+            this.neuesSpiel.UseVisualStyleBackColor = true;
+            this.neuesSpiel.Click += new System.EventHandler(this.neuesSpiel_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(715, 410);
+            this.Reset.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(256, 87);
+            this.Reset.TabIndex = 21;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Beenden
+            // 
+            this.Beenden.Location = new System.Drawing.Point(715, 509);
+            this.Beenden.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Beenden.Name = "Beenden";
+            this.Beenden.Size = new System.Drawing.Size(256, 87);
+            this.Beenden.TabIndex = 22;
+            this.Beenden.Text = "Beenden";
+            this.Beenden.UseVisualStyleBackColor = true;
+            this.Beenden.Click += new System.EventHandler(this.Beenden_Click);
+            // 
+            // A111
+            // 
+            this.A111.BackColor = System.Drawing.Color.White;
+            this.A111.Location = new System.Drawing.Point(252, 22);
+            this.A111.Name = "A111";
+            this.A111.Size = new System.Drawing.Size(224, 188);
+            this.A111.TabIndex = 23;
+            this.A111.UseVisualStyleBackColor = false;
+            this.A111.Click += new System.EventHandler(this.buttonClickt);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(1054, 638);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(57, 43);
+            this.pictureBox1.TabIndex = 26;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1114, 686);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.A111);
+            this.Controls.Add(this.Beenden);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.neuesSpiel);
+            this.Controls.Add(this.lbl_Gleichstand);
+            this.Controls.Add(this.lbl_Spieler1);
+            this.Controls.Add(this.lbl_spieler2);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.C0);
+            this.Controls.Add(this.B0);
+            this.Controls.Add(this.A0);
+            this.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A0;
+        
+        private System.Windows.Forms.Button B0;
+        private System.Windows.Forms.Button C0;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.TextBox lbl_spieler2;
+        private System.Windows.Forms.TextBox lbl_Spieler1;
+        private System.Windows.Forms.TextBox lbl_Gleichstand;
+        private System.Windows.Forms.Button neuesSpiel;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Beenden;
+        private System.Windows.Forms.Button A111;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Tic_tac_to
+{
+    public partial class Form1 : Form
+    {
+        public int spieler = 2;
+        public int zug = 0;
+        public int s1 = 0;
+        public int s2 = 0;
+        public int sd = 0;
+        Color BlackColor = Color.Black;
+        Color WhiteColor = Color.White;
+        bool IsDraw()
+        {
+            if (zug == 9 && IsWinner() == false)
+            {
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+        }
+        bool IsWinner()
+        {
+            //Horizontal
+            if (A0.Text == A111.Text && A111.Text == A2.Text && A0.Text != "")
+            {
+                Balken(1);
+                return true;
+                
+            }
+                
+            if (B0.Text == B1.Text && B1.Text == B2.Text && B0.Text != "")
+            {
+                Balken(2);
+                return true;
+            }
+
+            if (C0.Text == C1.Text && C1.Text == C2.Text && C0.Text != "")
+            {
+
+                Balken(3);
+                return true;
+            }
+
+            //Vertical
+            if (A0.Text == B0.Text && B0.Text == C0.Text && A0.Text != "")
+            {
+
+                Balken(4);
+                return true;
+            }
+            if (A111.Text == B1.Text && B1.Text == C1.Text && A111.Text != "")
+            {
+
+                Balken(5);
+                return true;
+            }
+            if (A2.Text == B2.Text && B2.Text == C2.Text && A2.Text != "")
+            {
+
+                Balken(6);
+                return true;
+            }
+
+            //diagonal
+            if (A0.Text == B1.Text && B1.Text == C2.Text && A0.Text != "")
+            {
+
+                Balken(7);
+                return true;
+            }
+            if (A2.Text == B1.Text && B1.Text == C0.Text && A2.Text != "")
+            {
+
+                Balken(8);
+                return true;
+            }
+            else
+                return false;
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl_Spieler1.Text = "Spieler 1: " + s1;
+            lbl_spieler2.Text = "Spieler 2: " + s2;
+            lbl_Gleichstand.Text = "Unentschieden" + sd;
+
+        }
+        private void buttonClickt(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if(button.Text == "")
+            {
+                if(spieler % 2 == 0)
+                {
+                    button.Text = "X";
+                    spieler++;
+                    zug++;
+                }
+                else
+                {
+                    button.Text = "O";
+                    spieler++;
+                    zug++;
+                }
+
+                if(IsDraw() == true)
+                {
+                    MessageBox.Show("Unentschieden");
+                    sd++;
+                    NewGame();
+                }
+
+                if(IsWinner() == true)
+                {
+                    if(button.Text == "X")
+                    {
+                        MessageBox.Show("Spieler 1 hat gewonnen");
+                        s1++;
+                        NewGame();
+                    }
+                    if (button.Text == "O")
+                    {
+                        MessageBox.Show("Spieler 2 hat gewonnen");
+                        s2++;
+                        NewGame();
+                    }
+
+                }
+            }
+
+        }
+
+        private void neuesSpiel_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+        public void NewGame()
+        {
+            spieler = 2;
+            zug = 0;
+            A0.Text = A111.Text = A2.Text = B0.Text = B1.Text = B2.Text = C0.Text = C1.Text = C2.Text = "";
+            lbl_Spieler1.Text = "spieler 1:" + s1;
+            lbl_spieler2.Text = "spieler 2:" + s2;
+            lbl_Gleichstand.Text = "Unentschieden:" + sd;
+
+            //balken
+            pictureBox1.Visible = false;
+
+            //Die Käseten bei einem Diagonalem Sieg werden wieder auf die Ausgangsituation zurückgesetzt
+            // also Weiß.
+            A0.BackColor = WhiteColor;
+            B1.BackColor = WhiteColor;
+            C2.BackColor = WhiteColor;
+            
+            A2.BackColor = WhiteColor;
+            B1.BackColor = WhiteColor;
+            C0.BackColor = WhiteColor;
+        }
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            s1 = s2 = sd = 0;
+            NewGame();
+        }
+
+        private void Beenden_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        
+        
+        //Nach dem Sieg wird der Pfad in dem die drei zeichen in einer Reihe platziert wurden
+        //der Balken eingebleden auf dem der positions Wert zutrifft.
+        //Wichtig kein Balken bei Diagonamel sie da man diesen nicht drehen kann.
+        private void Balken(int position)
+        {
+            switch(position)
+            {
+                //horizontal
+                case 1:
+                    pictureBox1.Location = new Point(75, 100);
+                    pictureBox1.Size = new Size(600, 25);
+                    pictureBox1.Visible = true;
+                    break;
+                case 2:
+                    pictureBox1.Location = new Point(75, 300);
+                    pictureBox1.Size = new Size(600, 25);
+                    pictureBox1.Visible = true;
+                    break;
+                case 3:
+                    pictureBox1.Location = new Point(75, 500);
+                    pictureBox1.Size = new Size(600, 25);
+                    pictureBox1.Visible = true;
+                    break;
+
+                //vertikal
+                case 4:
+                    pictureBox1.Size = new Size(25, 500);
+                    pictureBox1.Location = new Point(120, 45);
+                    pictureBox1.Visible = true;
+                    break;
+                case 5:
+                    pictureBox1.Size = new Size(25, 500);
+                    pictureBox1.Location = new Point(350, 45);
+                    pictureBox1.Visible = true;
+                    break;
+                case 6:
+                    pictureBox1.Size = new Size(25, 500);
+                    pictureBox1.Location = new Point(580, 45);
+                    pictureBox1.Visible = true;
+                    break;
+
+                //diagonal
+                case 7:
+                    A0.BackColor = BlackColor;
+                    B1.BackColor = BlackColor;
+                    C2.BackColor = BlackColor;
+                    break;
+                case 8:
+                    A2.BackColor = BlackColor;
+                    B1.BackColor = BlackColor;
+                    C0.BackColor = BlackColor;
+                    break;
+            }
+        }
+    }
+}
